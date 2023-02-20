1b00f5ad0b242a27c49eda736352014b2601266c
PullBeforePush <sonamaraxaba@gmail.com>
2023-02-20 19:27:46 +0200

    Initial commit

---
+++ Applications/src/main/java/Entity/User.java
@@ -0,0 +1,23 @@
+package Entity;
+
+import com.fasterxml.jackson.annotation.JsonTypeId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+      @Id
+      @GeneratedValue(strategy = GenerationType.IDENTITY)
+      private Long user_id;
+      private String user_name;
+      private String password;
+}
