fb860e9f90478126421d332e527d91157b721c24
PullBeforePush <sonamaraxaba@gmail.com>
2023-02-21 20:30:37 +0200

    Addede the Id and the one to one relationship to the Address entity

--- Applications/src/main/java/com/Bursary/Applications/Entity/AddressDetails.java
+++ Applications/src/main/java/com/Bursary/Applications/Entity/AddressDetails.java
@@ -10,9 +10,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AddressDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String municipality;
     private String surbub_or_town;
     private String area;
     private int house_number;
     private String street_name;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private User user;
 }
