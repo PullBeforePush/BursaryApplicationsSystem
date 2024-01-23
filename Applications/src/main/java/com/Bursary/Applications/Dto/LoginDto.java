577d8341a64ebdf9984be41de386cd88d6892711
PullBeforePush <sonamaraxaba@gmail.com>
2024-01-23 15:29:55 +0200

    These are the two login data transfer objects

---
+++ Applications/src/main/java/com/Bursary/Applications/Dto/LoginDto.java
@@ -0,0 +1,9 @@
+package com.Bursary.Applications.Dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+    private String usernammeOrEmail;
+    private String password;
+}
---
+++ Applications/src/main/java/com/Bursary/Applications/Dto/SignUpDto.java
@@ -0,0 +1,11 @@
+package com.Bursary.Applications.Dto;
+
+import lombok.Data;
+
+@Data
+public class SignUpDto {
+    private String name;
+    private String username;
+    private String email;
+    private  String password;
+}
